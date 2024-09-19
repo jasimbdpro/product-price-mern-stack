e741ea2c756d2886f52132cc374a48d584d3e683
Jasim Uddin <jasimbdpro@gmail.com>
2024-09-19 18:02:13 +0600

    api ready

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,42 @@ app.get('/', (req, res) => {
     res.status(200).send(productData)
 })
 
+app.get('/:id', (req, res) => {
+    const product = productData.find(product => product.id === parseInt(req.params.id))
+
+    if (!product) {
+        res.status(404).send('Product is not found')
+    }
+    res.status(200).send(product)
+    console.log(product)
+})
+
+app.put('/:id', (req, res) => {
+    const product = productData.find(product => product.id === parseInt(req.params.id))
+
+    if (!product) {
+        res.status(404).send('Product is not found')
+    }
+    Object.keys(req.body).forEach(key => {
+        product[key] = req.body[key];
+    })
+    res.status(200).send(product)
+    console.log(product)
+})
+
+app.delete('/:id', (req, res) => {
+    const index = productData.findIndex(product => product.id === parseInt(req.params.id))
+
+    if (!index) {
+        res.status(404).send('Product is not found')
+    }
+    productData.splice(index, 1)
+    res.status(200).send("Successfully Deleted")
+    console.log("Successfully Deleted")
+})
+
+
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
 })
