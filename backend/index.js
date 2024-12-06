7e996a843a15fc1ca4a321163139377ae38ce823
Jasim Uddin <jasimbdpro@gmail.com>
2024-12-06 10:50:50 +0600

    .

--- backend/index.js
+++ backend/index.js
@@ -1,99 +1,97 @@
-const express = require('express');
-const cors = require('cors')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-dotenv.config()
-const MONGO_URI = process.env.MONGO_URI;
-
+dotenv.config();
+const MONGO_URI_DB_FIRST_TEST = process.env.MONGO_URI_DB_FIRST_TEST;
 
 const app = express();
 const port = 3000;
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-mongoose
-    .connect(MONGO_URI)
-    .then(() => {
-        console.log("Connected to DB")
-    })
+mongoose.connect(MONGO_URI_DB_FIRST_TEST).then(() => {
+  console.log("Connected to DB");
+});
 
 const productSchema = new mongoose.Schema({
-    productName: String,
-    price: Number,
-})
-const ProductModel = mongoose.model('collection-2', productSchema)
+  productName: String,
+  price: Number,
+});
+const ProductModel = mongoose.model("collection-2", productSchema);
 
-app.post('/', async (req, res) => {
-    const { productName, price } = req.body; // Adjust these fields based on your user model
+app.post("/", async (req, res) => {
+  const { productName, price } = req.body; // Adjust these fields based on your user model
 
-    try {
-        // Create a new user instance
-        const newProduct = new ProductModel({ productName, price });
+  try {
+    // Create a new user instance
+    const newProduct = new ProductModel({ productName, price });
 
-        // Save the user to the database
-        await newProduct.save();
+    // Save the user to the database
+    await newProduct.save();
 
-        res.status(201).json({ message: 'Product created successfully', newProduct });
-    } catch (error) {
-        res.status(400).json({ message: 'Error creating product', error });
-    }
-})
+    res
+      .status(201)
+      .json({ message: "Product created successfully", newProduct });
+  } catch (error) {
+    res.status(400).json({ message: "Error creating product", error });
+  }
+});
 
-app.get('/', async (req, res) => {
-    try {
-        const productData = await ProductModel.find({});
-        res.status(200).json(productData);
-    } catch (error) {
-        console.error("Error fetching data:", error);
-        res.status(500).send("Error fetching data");
-    }
-})
+app.get("/", async (req, res) => {
+  try {
+    const productData = await ProductModel.find({});
+    res.status(200).json(productData);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).send("Error fetching data");
+  }
+});
 
-app.get('/:id', async (req, res) => {
-    const product = await ProductModel.findById(req.params.id);
+app.get("/:id", async (req, res) => {
+  const product = await ProductModel.findById(req.params.id);
+
+  if (!product) {
+    res.status(404).send("Product is not found");
+  }
+  res.status(200).send(product);
+  console.log(product);
+});
+
+app.put("/:id", async (req, res) => {
+  try {
+    const { productName, price } = req.body;
+    const product = await ProductModel.findByIdAndUpdate(
+      req.params.id,
+      { productName, price },
+      { new: true, runValidators: true }
+    );
 
     if (!product) {
-        res.status(404).send('Product is not found')
+      res.status(404).send("Product is not found");
     }
-    res.status(200).send(product)
-    console.log(product)
-})
 
-app.put('/:id', async (req, res) => {
-    try {
-        const { productName, price } = req.body;
-        const product = await ProductModel.findByIdAndUpdate(req.params.id,
-            { productName, price },
-            { new: true, runValidators: true }
-        );
+    res.status(200).send(product);
+    console.log(product);
+  } catch (error) {
+    res.status(400).json({ message: "Error updating product", error });
+  }
+});
 
-        if (!product) {
-            res.status(404).send('Product is not found')
-        }
+app.delete("/:id", async (req, res) => {
+  try {
+    const product = await ProductModel.findByIdAndDelete(req.params.id);
 
-        res.status(200).send(product)
-        console.log(product)
-    } catch (error) {
-        res.status(400).json({ message: 'Error updating product', error });
+    if (!product) {
+      res.status(404).json({ message: "Product not found" });
     }
-})
-
-app.delete('/:id', async (req, res) => {
-    try {
-        const product = await ProductModel.findByIdAndDelete(req.params.id);
-
-        if (!product) {
-            res.status(404).json({ message: 'Product not found' })
-        }
-        res.status(200).send("Successfully Deleted")
-    } catch (error) {
-        res.status(500).json({ message: 'Error deleting product', error });
-    }
-})
-
-
+    res.status(200).send("Successfully Deleted");
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting product", error });
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`)
-})
+  console.log(`Server is running on port: ${port}`);
+});
